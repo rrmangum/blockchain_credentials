a0c15e9fb00c24435ce6039cb95646deca2a4129
miltiades-the-general <miltiadesgeneral@gmail.com>
2022-11-04 16:51:54 -0700

    Styling changes

--- flaskapp_ben_changes/app/user/views.py
+++ flaskapp_ben_changes/app/user/views.py
@@ -2,7 +2,7 @@ from . import user_blueprint
 from flask import render_template, request, redirect, url_for
 from ..extensions import db
 from ..models import User
-from forms import MyForm
+# from forms import MyForm
 
 from flask_wtf import FlaskForm
 from wtforms import StringField
