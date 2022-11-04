dd6b311378e6efb2f9ff3f8b404d5bda446d42c8
miltiades-the-general <miltiadesgeneral@gmail.com>
2022-11-04 16:26:26 -0700

    Achieved form to database functionality

--- flaskapp_ben_changes/app/user/views.py
+++ flaskapp_ben_changes/app/user/views.py
@@ -2,8 +2,7 @@ from . import user_blueprint
 from flask import render_template, request, redirect, url_for
 from ..extensions import db
 from ..models import User
-import random
-# from forms import MyForm
+from forms import MyForm
 
 from flask_wtf import FlaskForm
 from wtforms import StringField
@@ -18,24 +17,24 @@ class MyForm(FlaskForm):
 def index():
     return render_template('/index.html')
 
-# Handles form submission 
-# @user_blueprint.route("/submit", methods=['GET', 'POST'])
-# def submit():
-#     form = MyForm()
-#     if form.validate_on_submit():
-#         username = request.form["username"]
-#         email = request.form["email"]
-#         id = random.randint(1, 1000000)
-#         entry = User(id, username, email)
-#         db.session.add(entry)
-#         db.session.commit()
-#         return redirect('/success')
-#     return render_template('index.html', form=form)
-
-@user_blueprint.route("/submit", methods=['GET', 'POST'])
+# Handles form submission
+# Posts user info to the database
+@user_blueprint.route("/submit", methods=['POST'])
 def submit():
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-            
-        return f"{username}, {email}"
+    username = request.form['username']
+    email = request.form['email']
+    form = MyForm()
+    # if form.validate_on_submit():
+    #     username = request.form['username']
+    #     email = request.form['email']
+    #     id = random.randint(1, 1000000)
+    #     entry = User(id, username, email)
+    #     db.session.add(entry)
+    #     db.session.commit()
+    #     return redirect('/success')
+    username = request.form['username']
+    email = request.form['email']
+    entry = User(name=username, email=email)
+    db.session.add(entry)
+    db.session.commit()
+    return render_template('success.html', form=form)
