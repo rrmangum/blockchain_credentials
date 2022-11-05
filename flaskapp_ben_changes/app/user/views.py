e999478f72115a46280055e06f0e97fbdd017d4a
miltiades-the-general <miltiadesgeneral@gmail.com>
2022-11-05 16:47:15 -0700

    Added wtf flask functionality to forms to validate on submit

--- flaskapp_ben_changes/app/user/views.py
+++ flaskapp_ben_changes/app/user/views.py
@@ -5,36 +5,50 @@ from ..models import User
 # from forms import MyForm
 
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Email
 
 # Class for form 
 class MyForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired()])
-    email = StringField('email', validators=[Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[Email()])
+    submit = SubmitField('Submit')
 
-@user_blueprint.route('/')
-def index():
-    return render_template('/index.html')
+# @user_blueprint.route('/')
+# def index():
+#     return render_template('/index.html')
 
 # Handles form submission
 # Posts user info to the database
-@user_blueprint.route("/submit", methods=['POST'])
+# @user_blueprint.route("/submit", methods=['POST'])
+# def submit():
+#     username = request.form['username']
+#     email = request.form['email']
+#     form = MyForm()
+#     form.username = username
+#     form.email = email
+#     if form.validate_on_submit():
+#         entry = User(username, email)
+#         db.session.add(entry)
+#         db.session.commit()
+#         return render_template('success.html', form=form)
+#     return render_template('index.html')
+
+@user_blueprint.route("/", methods=['GET', 'POST'])
 def submit():
-    username = request.form['username']
-    email = request.form['email']
+    username = None
+    email = None
     form = MyForm()
-    # if form.validate_on_submit():
-    #     username = request.form['username']
-    #     email = request.form['email']
-    #     id = random.randint(1, 1000000)
-    #     entry = User(id, username, email)
-    #     db.session.add(entry)
-    #     db.session.commit()
-    #     return redirect('/success')
-    username = request.form['username']
-    email = request.form['email']
-    entry = User(name=username, email=email)
-    db.session.add(entry)
-    db.session.commit()
-    return render_template('success.html', form=form)
+
+    if form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        username = ''
+        email = ''
+
+    return render_template('index.html', 
+        username = username,
+        email = email,
+        form = form
+    )
+    
