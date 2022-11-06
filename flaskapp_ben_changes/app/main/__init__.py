08ea89b4e93595a5916d74b7c820adc76d2e423b
miltiades-the-general <miltiadesgeneral@gmail.com>
2022-11-06 10:38:50 -0800

    Form functionality, moved to flaskapp directory

--- flaskapp_ben_changes/app/main/__init__.py
+++
@@ -1,7 +0,0 @@
-#This bluepint will deal with all homepage functionality 
-
-from flask import Blueprint
-
-main_blueprint = Blueprint('main', __name__, template_folder='templates')
-
-from . import views
