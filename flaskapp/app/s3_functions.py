e7233221ba4e14dd1b4c53e24626be55862ee16b
Greg Richardson <jgrichardson67@yahoo.com>
2022-11-07 18:25:24 -0500

    Fix compromised keys, moved to env

--- flaskapp/app/s3_functions.py
+++ flaskapp/app/s3_functions.py
@@ -1,8 +1,12 @@
 import boto3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 s3 = boto3.client('s3',
-                    aws_access_key_id='AKIAS64VQUZTEAL4F3PX',
-                    aws_secret_access_key= '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'
+                    aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
+                    aws_secret_access_key= os.getenv('AWS_SECRET_ACCESS_KEY')
                      )
 BUCKET_NAME='blockchain-identity'
 
