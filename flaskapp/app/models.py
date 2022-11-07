0000958ff6c6cb3c5bb54d2da9cb9105d63028cf
miltiades-the-general <miltiadesgeneral@gmail.com>
2022-11-07 13:24:57 -0800

    Wallet registration

--- flaskapp/app/models.py
+++ flaskapp/app/models.py
@@ -4,7 +4,7 @@ from .extensions import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(50), unique=True, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     
 class Wallet(db.Model):
