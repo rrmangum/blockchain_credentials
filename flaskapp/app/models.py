e18de6fdf5145e4af994d35f33bf591a3e10351e
miltiades-the-general <miltiadesgeneral@gmail.com>
2022-11-10 18:55:04 -0800

    Hide profile button before login

--- flaskapp/app/models.py
+++ flaskapp/app/models.py
@@ -34,6 +34,7 @@ class Credential(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     wallet_id = db.Column(db.Integer, db.ForeignKey('wallet.id'))
     name = db.Column(db.String(50), nullable=False)
+    image_url = db.Column(db.String(200), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     
 class Issuance(db.Model):
