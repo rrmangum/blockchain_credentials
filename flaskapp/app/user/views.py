56f651ce8bddd155364bcbb54dbc1274da6964be
miltiades-the-general <miltiadesgeneral@gmail.com>
2022-11-12 13:03:37 -0800

    streamlining profile page

--- flaskapp/app/user/views.py
+++ flaskapp/app/user/views.py
@@ -8,61 +8,74 @@ from flask_login import login_required
 
 # Handles form and form submission, validation requires a valid email address and data for each form field
 # Form is pushed to the sql database
+# @user_blueprint.route("/", methods=['GET', 'POST'])
+# @login_required
+# def new_user():
+#     user = User.query.get(current_user.id)
+#     if not user.name:
+#         request.method = "POST"
+#     if request.method == "POST":
+#         username = None
+#         email = None
+#         profile_image = None
+#         created_at = current_user.created_at
+#         wallet = Wallet.query.filter_by(user_id=current_user.id).first()
+
+#         form = MyForm()
+
+#         if form.validate_on_submit():
+#             username = form.username.data
+#             email = form.email.data
+#             profile_image = form.profile_image.data
+
+#             current_user.name = username
+#             current_user.email = email
+#             current_user.profile_image = profile_image
+            
+
+#             # img = request.files['file']
+#             # filename = secure_filename(img.filename)
+#             # img.save(filename)
+#             # s3.upload_file(
+#             #     Bucket = os.environ['S3_PROFILE_PIC_BUCKET_NAME'],
+#             #     Filename=filename,
+#             #     Key = filename
+#             # )
+#             # full_filename = f"https://{os.environ['S3_PROFILE_PIC_BUCKET_NAME']}.s3.amazonaws.com/{filename}"
+
+
+#             db.session.commit()
+#             flash("User Profile Updated")
+#             return redirect("/users")
+#         return render_template("edit.html", form=form, username=username, email=email, profile_image=profile_image, created_at=created_at, wallet=wallet)
+
+
+#         # return render_template('new.html', 
+#         #     username = username,
+#         #     email = email,
+#         #     profile_image=profile_image,
+#         #     form = form
+#         # )
+#     elif request.method == "GET":
+#         wallet = Wallet.query.filter_by(user_id=current_user.id).first()
+#         credentials = Credential.query.filter_by(wallet_id=wallet.id).first()
+#         username = current_user.name
+#         email = current_user.email
+#         profile_image = current_user.profile_image
+#         created_at = current_user.created_at
+#         return render_template('profile.html', username=username, email=email, profile_image=profile_image, created_at=created_at, wallet=wallet)
+
 @user_blueprint.route("/", methods=['GET', 'POST'])
 @login_required
 def new_user():
-    user = User.query.get(current_user.id)
-    if not user.name:
-        request.method = "POST"
-    if request.method == "POST":
-        username = None
-        email = None
-        profile_image = None
-        form = MyForm()
+    wallet = Wallet.query.filter_by(user_id=current_user.id).first()
+    credentials = Credential.query.filter_by(wallet_id=wallet.id).first()
+    username = current_user.name
+    email = current_user.email
+    profile_image = current_user.profile_image
+    created_at = current_user.created_at
+    return render_template('profile.html', username=username, email=email, profile_image=profile_image, created_at=created_at, wallet=wallet)
 
-        if form.validate_on_submit():
-            username = form.username.data
-            email = form.email.data
-            profile_image = form.profile_image.data
-            current_user.name = username
-            current_user.email = email
-            current_user.profile_image = profile_image
-
-            # img = request.files['file']
-            # filename = secure_filename(img.filename)
-            # img.save(filename)
-            # s3.upload_file(
-            #     Bucket = os.environ['S3_PROFILE_PIC_BUCKET_NAME'],
-            #     Filename=filename,
-            #     Key = filename
-            # )
-            # full_filename = f"https://{os.environ['S3_PROFILE_PIC_BUCKET_NAME']}.s3.amazonaws.com/{filename}"
-
-
-            db.session.commit()
-            username = ''
-            email = ''
-            profile_image = ''
-            flash(f"User {username} Created")
-            return render_template('success.html', form = form)
-        else:
-            flash("Invalid Entry")
-
-
-        return render_template('new.html', 
-            username = username,
-            email = email,
-            profile_image=profile_image,
-            form = form
-        )
-    elif request.method == "GET":
-        wallet = Wallet.query.filter_by(user_id=current_user.id).first()
-        credentials = Credential.query.filter_by(wallet_id=wallet.id).first()
-        username = current_user.name
-        email = current_user.email
-        profile_image = current_user.profile_image
-        created_at = current_user.created_at
-        return render_template('profile.html', username=username, email=email, profile_image=profile_image, created_at=created_at, wallet=wallet)
 
 @user_blueprint.route("/edit", methods=["GET", "POST"])
 @login_required
