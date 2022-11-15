89d0fee46840d38302092271b9d5abd655f19906
Garrett <gtkhhz@gmail.com>
2022-11-14 19:02:59 -0800

    Got send_credential working on pragma 0.8, adjusted comments

--- contracts/send_credential_p8.sol
+++ contracts/send_credential_p8.sol
@@ -4,19 +4,19 @@ pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
 
 // required for totalSupply() function
-// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+
 // Credentials inherit ERC721
 contract Credential is ERC721 {
     // A constructor that sets the initial value of TokenId as well as calls ERC721 Constructor
-    constructor() public ERC721("Credential", "CRED") {
-        uint256 tokenId = 0;
-    }
+    constructor() public ERC721("Credential", "CRED") {}
+
 
     // Public funtion anyone can call. Returns TokenID
     function BestowCredential(address owner, string memory tokenURI) public returns (uint256){
-        uint256 tokenId = tokenId ++;
+        uint256 tokenId = totalSupply();
         _mint(owner, tokenId);
 
-        return tokenId;
+        return tokenId; 
     }
 }
