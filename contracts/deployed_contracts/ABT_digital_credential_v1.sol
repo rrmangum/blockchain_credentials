1ee5ecc80151dc56213b2ef015f0801a7f5d480d
Ryan <rrmangum@gmail.com>
2022-11-16 18:43:39 -0800

    fixed some typos

--- contracts/deployed_contracts/ABT_digital_credential_v1.sol
+++ contracts/deployed_contracts/ABT_digital_credential_v1.sol
@@ -5,14 +5,21 @@ import "@openzeppelin/contracts@4.7.0/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts@4.7.0/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts@4.7.0/access/Ownable.sol";
 import "@openzeppelin/contracts@4.7.0/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
-contract DigitalCredential is ERC721, ERC721URIStorage, Ownable {
+contract DigitalCredential is
+    ERC721,
+    ERC721URIStorage,
+    Ownable,
+    ERC721Burnable
+{
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
     constructor() ERC721("DigitalCredential", "DC") {}
 
+    // anyone can call safeMint function
     function safeMint(address to, string memory uri) public {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
