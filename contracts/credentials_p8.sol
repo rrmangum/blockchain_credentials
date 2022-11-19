ee44d93d82f3ce8b77893a972f08245e066ab4fb
Garrett <gtkhhz@gmail.com>
2022-11-19 13:46:56 -0800

    Merge Ryan and Garrett Smart contracts

--- contracts/credentials_p8.sol
+++ contracts/credentials_p8.sol
@@ -1,37 +1,40 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts@4.7.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.7.0/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@4.7.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.7.0/utils/Counters.sol";
 
+contract DigitalCredential is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
 
-// Credentials inherit ERC721URIStorage and ERC721Enumerable for totalSupply() and _setTokenURI functions
-contract Credentials is ERC721URIStorage, ERC721Enumerable {
-    // A constructor that sets the initial value of TokenId as well as calls ERC721 Constructor
+    ///// function imports from OpenZeppelin contracts wizard ///
+
+    // The counter assigns tokenId as they are minted
+    Counters.Counter private _tokenIdCounter;
+
+    // This event is emitted when a token is created and bound to an address
+    event Attest(address indexed to, uint256 indexed tokenId);
+
+    //This event is emitted when a token is removed
+    event Revoke(address indexed to, uint256 indexed tokenId);
+
+    // Create Credential ERC721 token
     constructor() ERC721("Credential", "CRED") {}
 
-
-    // Public funtion anyone can call. Returns TokenURI
-    function BestowCredential(address owner, string memory token_URI) public returns (string memory){
-        uint256 tokenId = totalSupply();
-        string transactionHash = _mint(owner, tokenId);
-        _setTokenURI(tokenId, token_URI);
-
-        return token_URI; 
+    // anyone can call safeMint function
+    function BestowCredential(address to, string memory uri) public {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
     }
-    // The following functions are overrides required by Solidity.
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+    function _burn(uint256 tokenId)
         internal
-        override(ERC721, ERC721Enumerable)
-    {   require(from == address(0), "This credential cannot be transferred.");
-        super._beforeTokenTransfer(from, to, tokenId, batchSize);
-    }
-
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        override(ERC721, ERC721URIStorage)
+    {
         super._burn(tokenId);
     }
 
@@ -44,12 +47,45 @@ contract Credentials is ERC721URIStorage, ERC721Enumerable {
         return super.tokenURI(tokenId);
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
+    ///// VITAE contract specific functions /////
+
+    // This function allows the owner of the token to burn it
+    function deleteCredential(uint256 tokenId) public {
+        require(
+            ownerOf(tokenId) == msg.sender,
+            "Only the owner or the issuer of the credential can delete it."
+        );
+        _burn(tokenId);
+    }
+
+    // This function allows the contract owner(us) to burn the token. As written, only the contract creator(us) can revoke the credential
+    // TODO: This function should also be callable by the issuer, create a modifier requiring msg.sender to be verified as tokenId issuer address
+    function revokeCredential(uint256 tokenId) external onlyOwner {
+        _burn(tokenId);
+    }
+
+    // This function stops the user from transferring the token
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256
+    ) internal pure override {
+        require(
+            from == address(0) || to == address(0),
+            "This credential cannot be transferred."
+        );
+    }
+
+    // This function emits the events to store the data
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override {
+        if (from == address(0)) {
+            emit Attest(to, tokenId);
+        } else if (to == address(0)) {
+            emit Revoke(to, tokenId);
+        }
     }
 }
