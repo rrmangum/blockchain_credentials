1fbcf8ef59146efb9bc8e5027e5c43abfcd671ee
miltiades-the-general <miltiadesgeneral@gmail.com>
2022-11-20 19:09:30 -0800

    Revoke added to functionality

--- flaskapp/app/issuance/views.py
+++ flaskapp/app/issuance/views.py
@@ -2,16 +2,28 @@ from . import issuance_blueprint
 from flask import render_template, request, redirect, url_for, flash, jsonify
 from ..extensions import db
 from ..models import Credential
-from ..models import Wallet
 from ..models import Issuance
+from .forms import RevokeForm
 
-@issuance_blueprint.route("/<int:credential_id>", methods=['GET'])
+@issuance_blueprint.route("/<int:credential_id>", methods=['GET', 'POST'])
 def index(credential_id):
+    form = RevokeForm()
+
     if request.method == 'GET':
         credential = Credential.query.get(credential_id)
+        issuances = credential.issuances 
+        return render_template("issuance/index.html", issuances = issuances, credential = credential, form=form)
+
+    elif request.method == 'POST':
+        credential = Credential.query.get(credential_id)
         issuances = credential.issuances
-        return render_template("issuance/index.html", issuances = issuances, credential = credential)
 
+        if form.validate_on_submit():     
+            # Request the checked issuances 
+            issuance_ids = request.form.getlist('issuanceCheckbox')
 
-# @issuance_blueprint.route("/individuals", methods=['GET'])
-# def individuals()
+            # Delete the issuances that the administrator checked
+            for issuance in issuance_ids:
+                Issuance.query.filter_by(id=int(issuance)).delete()
+            db.session.commit()
+    return render_template("issuance/index.html", issuances = issuances, credential = credential, form=form)
